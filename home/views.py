8cabd397406790c1d31b9e27c7cdb01979ff10ca
Daniel <daniel.ctpr@gmail.com>
2025-05-27 17:50:42 +0100

    changed the featured and bestselling books condition to a manual input for testing purposes

--- home/views.py
+++ home/views.py
@@ -4,13 +4,17 @@ from django.db.models import Avg
 
 def home(request):
     # Featured books
-    featured_books = Book.objects.all().annotate(avg_rating=Avg('reviews__rating')).order_by('-created_at')[:6]
+    featured_book_ids = [20, 21, 22, 25, 26, 30]
+    featured_books = Book.objects.filter(id__in=featured_book_ids).annotate(avg_rating=Avg('reviews__rating'))
+    featured_books = sorted(featured_books, key=lambda x: featured_book_ids.index(x.id))
     
     # Top categories
     categories = Category.objects.filter(parent__isnull=True)[:6]
     
     # Best-selling books
-    bestsellers = Book.objects.all().annotate(avg_rating=Avg('reviews__rating')).order_by('?')[:6]
+    bestseller_ids = [28, 29, 26, 22, 24, 23]
+    bestsellers = Book.objects.filter(id__in=bestseller_ids).annotate(avg_rating=Avg('reviews__rating'))
+    bestsellers = sorted(bestsellers, key=lambda x: bestseller_ids.index(x.id))
     
     context = {
         'featured_books': featured_books,
